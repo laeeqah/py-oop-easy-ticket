acbed8cac0948bfde5d8f439fb11a8678bb126bc
laeeqah <71064103+laeeqah@users.noreply.github.com>
2020-10-28 08:47:42 +0200

    Add files via upload

--- py-oop-easy-ticket/main.py
+++ py-oop-easy-ticket/main.py
@@ -7,7 +7,7 @@ from tkinter import ttk
 window = Tk()
 window.geometry("500x500")
 window.title("Ticket Sales")
-
+window.configure(background= "green")
 frame = Frame(window)
 
 l1 = Label(window, text = "Enter CellNumber: " ,justify = "left")
@@ -15,11 +15,13 @@ l1.pack(side = LEFT)
 l1.place(x= 1, y= 1)
 
 
-e1 = Entry(window, bd = 1)
-e1.pack(side = RIGHT)
-e1.place(x = 200, y = 1)
+cellnum = Entry(window, bd = 1)
+cellnum.pack(side = RIGHT)
+cellnum.place(x = 200, y = 1)
 
 #Making label and combobox
+var2 = StringVar()
+var2.set("Select Ticket")
 
 l2 = Label(window, text = "Select Ticket Category: ",justify = "left")
 l2.pack(side = LEFT)
@@ -27,12 +29,13 @@ l2.place(x = 1, y = 50)
 
 var2 = StringVar()
 var2.set("Select Ticket")
-drop = ttk. Combobox(window, value =("Soccer", "Movie", "Theater") , textvariable = var2)
-drop.pack(side = RIGHT)
-drop.place(x = 200, y = 50)
+tprice = ttk. Combobox(window, value =("Soccer", "Movie", "Theater") , textvariable = var2)
+tprice.pack(side = RIGHT)
+tprice.place(x = 200, y = 50)
 
 #Making a spinbox with label
-
+var = IntVar()
+var.set(0)
 
 l3 = Label(window, text = "Number of Tickets Bought: ", justify= "left")
 l3.pack(side = LEFT)
@@ -40,55 +43,80 @@ l3.place(x = 1, y = 100)
 
 var = IntVar()
 var.set(0)
-box = Spinbox(window, from_=0, to=100, width = 5, textvariable = var)
-box.pack(side = RIGHT)
-box.place(x = 200, y = 100)
+nr_tickets = Spinbox(window, from_=0, to=30, width = 5, textvariable = var)
+nr_tickets.pack(side = RIGHT)
+nr_tickets.place(x = 200, y = 100)
 
 #Buttons and calculating
-btn1 = Button(window, text = "Calculate Ticket")
+
+
+class clsTicketSales():
+    def ___init__(self, cellnum, tprice, nr_tickets):
+        self.cellnum = cellnum
+        self.tprice = tprice
+        self.nr_tickets = nr_tickets
+        return
+
+
+def CalcPrepayment():
+
+    self = tprice.get()
+    if  self == "Soccer":
+        priceOfTickets = 40
+    elif self == "Movie":
+        priceOfTickets = 75
+    elif self == "Theater":
+        priceOfTickets = 100
+    else:
+        displayLabel = Label(window, text="Please select what ticket you would like")
+        displayLabel.pack(side =LEFT)
+        displayLabel.place(x = 1, y = 250)
+
+
+    ticketAmt = nr_tickets.get()
+    if ticketAmt == "0":
+        displayLabel = Label(window, text="Please select how much tickets you would like")
+        displayLabel.pack(side =LEFT)
+        displayLabel.place(x = 1, y = 400)
+    else:
+        totalPrice = float(ticketAmt) * float(priceOfTickets) + 0.14
+        strTotalPrice = str(totalPrice)
+        displayLabel = Label(window, text="Amount Payable: R" + strTotalPrice + "\n" + "Reservation for " + self + " for " + ticketAmt + "\n" + "was done by " + cellnum.get())
+        displayLabel.pack(side =LEFT)
+        displayLabel.place(x = 1, y = 300)
+
+    var = IntVar()
+    var.set()
+    nr_tickets.config(textvariable = var)
+    var2 = StringVar()
+    var2.set("soccer" ,"Movie","theater")
+    tprice.config(textvariable = var2)
+    displayLabel = Label()
+    displayLabel.set(self)
+    displayLabel.config(totalPrice)
+    
+    
+
+    
+btn1 = Button(window, text = "Calculate Ticket", command = CalcPrepayment)
 btn1.pack(side = RIGHT)
 btn1.place(x = 1, y = 150)
-
-class clsticketsales:
-    def __init__(self):
-        self. soccer = 40
-        self. movie = 75
-        self. theater = 100
-
-class CellNumber(clsticketsales):
-    def __init__(self):
-        self.cellnumber =
-
-def AmountPayable(ticketsales):
-    AmountPayable=nr_tickets * tPrice + 14% VAT
-
-
-
+    
 
 #clear everything
 def clear_all():
-    e1.delete(0,END)
+    cellnum.delete(0,END)
     var = IntVar()
     var.set(0)
-    box.config(textvariable = var)
+    nr_tickets.config(textvariable = var)
     var2 = StringVar()
     var2.set("")
-    drop.config(textvariable = var2)
+    tprice.config(textvariable = var2)
 
 
 btn2 = Button(window, text = "Clear Entries", command = clear_all)
 btn2.pack( side= LEFT)
 btn2.place(x = 200, y = 150 )
-
-
-
-
-
-
-
-
-
-
 
 
 window.mainloop()
